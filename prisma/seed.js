cbd08063093674fd558cb93353c0e11b55b6e04c
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-24 19:54:33 -0400

    3 users with 5 reviews each

--- prisma/seed.js
+++ prisma/seed.js
@@ -58,30 +58,103 @@ const seed = async () => {
   })
 
   console.log(`SHOWS CREATED`);
-  console.log(`CREATING USERS`);
+  console.log(`CREATING USERS AND REVIEWS`);
 
   await prisma.user.create({
     data: {
       username: `wyatt`,
       password: `wyatt1111`,
       email: `wyatt@gmail.com`,
-      image: `wyattr.jpg`,
+      image: `wyatt.jpg`,
       isAdmin: true,
       reviews: {
         create: [
           { show_id: 1,
             rating: 7,
-            content: `test`
+            content: `I love this show`
            },
            { show_id: 2,
-            rating: 7,
-            content: `test`
+            rating: 9,
+            content: `I really love this show`
+           },
+           { show_id: 3,
+            rating: 8,
+           },
+           { show_id: 4,
+            rating: 9,
+           },
+           { show_id: 5,
+            rating: 10,
            }
         ]
       }
-
     }
   })
+
+  await prisma.user.create({
+    data: {
+      username: `tom`,
+      password: `tom1111`,
+      email: `tom@gmail.com`,
+      image: `tom.jpg`,
+      isAdmin: true,
+      reviews: {
+        create: [
+          { show_id: 6,
+            rating: 7,
+            content: `I love this show`
+           },
+           { show_id: 7,
+            rating: 4,
+            content: `I really love this show`
+           },
+           { show_id: 1,
+            rating: 8,
+           },
+           { show_id: 2,
+            rating: 7,
+           },
+           { show_id: 3,
+            rating: 6,
+           }
+        ]
+      }
+    }
+  })
+
+  await prisma.user.create({
+    data: {
+      username: `david`,
+      password: `david1111`,
+      email: `david@gmail.com`,
+      image: `david.jpg`,
+      isAdmin: true,
+      reviews: {
+        create: [
+          { show_id: 4,
+            rating: 7,
+            content: `I love this show`
+           },
+           { show_id: 6,
+            rating: 8,
+            content: `I love this show`
+           },
+           { show_id: 5,
+            rating: 8,
+           },
+           { show_id: 1,
+            rating: 7,
+           },
+           { show_id: 2,
+            rating: 6,
+           }
+        ]
+      }
+    }
+  })
+
+  console.log(`USERS AND REVIEWS CREATED`);
+  console.log(`CREATING COMMENTS`);
 
 };
 
