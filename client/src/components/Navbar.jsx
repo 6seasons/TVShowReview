46cc123812315732982ecf3fc75786865977fdae
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-25 14:36:24 -0400

    Navbar setup with Home route, 2 dummy routes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
   return (
     <>
-      <h1>Navbar</h1>
+      <section>
+        <Link to='/'>Home</Link>
+        <Link to='/test'>Show List</Link>
+      </section>
+      <section>
+        <Link to='/logintest'>Log In</Link>
+      </section>
     </>
   )
 }
