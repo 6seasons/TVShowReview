f10055bab54d771a1233f0229bcdedae1129a3d6
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-08-25 10:06:52 -0700

    home page creation

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+const HomePage = () => {
+    console.log("homepage")
+    return (
+        <>
+        <h1>6 Seasons Review</h1>
+        </>
+    )
+};
+
+export default HomePage
