f1693734f0bcebee8f1106dedc4172c211d5b935
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-28 16:27:26 -0400

    Token working, /me route returns user info with reviews and comments

--- server/api/users.js
+++ server/api/users.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const { checkAuth } = require('./utils')
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello from users");
+router.get("/me", checkAuth, async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: req.userID },
+    include: {
+      reviews: true,
+      comments: true
+    }
+  })
+  res.send(user);
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -2,13 +2,28 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const api = require("./api");
+const auth = require("./auth");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const auth = req.headers.authorization;
+  const token = auth?.startsWith("Bearer") ? auth.slice(7) : null;
+  try {
+    const { id } = jwt.verify(token, process.env.JWT);
+    req.userID = id;
+  } catch (err) {
+    req.userID = null;
+  }
+  next();
+});
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
@@ -17,6 +32,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", api);
+app.use("/auth", auth);
 
 app.listen(PORT, (error) => {
   if (!error) {
