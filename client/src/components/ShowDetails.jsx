7dceb37c80de25249a81a6ce235cd3971cac4a83
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-28 12:11:13 -0400

    fixing node modules

--- client/src/components/ShowDetails.jsx
+++ client/src/components/ShowDetails.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+
 const ShowDetails = () => {
+
+  const [show, setShow] = useState({});
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchShow = async () => {
+      try {
+        const response = await fetch('/api/shows/5');
+        const data = await response.json();
+        console.log(data.show);
+        console.log(data.reviews);
+        setShow(data.show);
+        setReviews(data.reviews);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchShow();
+  }, [])
+
   return (
     <>
-      <h1>show details</h1>
+      <h1>hello</h1>
     </>
   );
 };
