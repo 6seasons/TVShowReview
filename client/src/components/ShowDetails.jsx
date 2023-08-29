bbd6b4a39b8920fa9e8f742eb152f3e3d63356d5
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-29 15:41:36 -0400

    Searchbar returns show results and takes you to show page

--- client/src/components/ShowDetails.jsx
+++ client/src/components/ShowDetails.jsx
@@ -2,9 +2,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const ShowDetails = () => {
-
   const { showID } = useParams();
-  
+
   const [show, setShow] = useState(null);
   const [reviews, setReviews] = useState([]);
 
@@ -16,35 +15,34 @@ const ShowDetails = () => {
         setShow(data.show);
         setReviews(data.reviews);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
+    };
     fetchShow();
-  }, [])
-  
+  }, [showID]);
+
   console.log(show);
   console.log(reviews);
-  
+
   return (
     <>
-      {show ?
-      <section>
-        <h1>{show.name}</h1>
-        {/* <img src={show.imageUrl} /> */}
-        <p>{show.details}</p>
-      </section> :
-      null
-      }
-      {reviews.map(review => {
+      {show ? (
+        <section>
+          <h1>{show.name}</h1>
+          {/* <img src={show.imageUrl} /> */}
+          <p>{show.details}</p>
+        </section>
+      ) : null}
+      {reviews.map((review) => {
         return (
           <>
-          <section>
-            <h3>{review.user.username}</h3>
-            <h1>{review.rating}</h1>
-            <p>{review.content? review.content : null}</p>
-          </section>
+            <section>
+              <h3>{review.user.username}</h3>
+              <h1>{review.rating}</h1>
+              <p>{review.content ? review.content : null}</p>
+            </section>
           </>
-        )
+        );
       })}
     </>
   );
