ebaf6b8f0a35e5beeaa235f5c028a5162ad2ee8e
David Arcibar <davidaarcibar@gmail.com>
2023-08-29 10:26:24 -0500

    made two new routes

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,24 +1,62 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 require("dotenv").config();
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if user already exists
+    const userExists = await prisma.user.findUnique({ where: { username } });
+    if (userExists) {
+      return res.status(400).json({ error: 'Username already exists.' });
+    }
+
+    // Hash the password before saving it
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Save the user to the database
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            password: hashedPassword
+        }
+    });
+
+    res.status(201).json({ message: 'User registered successfully', userId: newUser.id });
+
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong. Please try again.' });
+  }
+});
+
 router.post("/login", async (req, res) => {
   try {
-    const user = await prisma.user.findUnique({
-      where: { username: req.body.username },
-    });
-    if (user.password === req.body.password) {
-      const token = jwt.sign({ id: user.id }, process.env.JWT);
-      res.send(token);
-    } else {
-      res.send("something went wrong");
+    const { username, password } = req.body;
+
+    // Check if user exists
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      return res.status(404).json({ error: 'Username not found.' });
     }
-  } catch (err) {
-    console.log(err);
+
+    // Compare entered password with stored hashed password
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ error: 'Invalid password.' });
+    }
+
+    // If valid, generate a JWT
+    const token = jwt.sign({ id: user.id }, process.env.JWT);
+    res.send(token);
+
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong. Please try again.' });
   }
 });
 
