9a5a9764cf0ef0226ef2b9e29166530b4fa17546
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-30 16:20:35 -0400

    Review Page displaying data from API

--- client/src/components/ReviewPage.jsx
+++ client/src/components/ReviewPage.jsx
@@ -1,11 +1,29 @@
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const ReviewPage = () => {
+  const [review, setReview] = useState();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchReview = async () => {
+      try {
+        const response = await fetch(`/api/reviews/${id}`);
+        const data = await response.json();
+        setReview(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchReview();
+  }, []);
+
   return (
     <>
-      <h1>Review Page</h1>
+      <h1>{review ? review.user.username : null}</h1>
     </>
-  )
-}
+  );
+};
 
-export default ReviewPage
+export default ReviewPage;
